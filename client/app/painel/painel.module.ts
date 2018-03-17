d86ecd8027093f1a34eedb6d4af6d1462e1d3815
Fabio Leforte <fmatias@indracompany.com>
2018-03-17 12:29:45 -0300

    atualização do modulo painel

---
+++ client/app/painel/painel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { PainelComponent } from './painel.component';
+
+
+
+@NgModule({
+  
+    declarations: [PainelComponent],
+    exports:[PainelComponent],
+    providers: [],
+})
+export class PainelModule { }

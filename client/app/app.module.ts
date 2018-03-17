43aa7af52362989e66960588174c1bf09222f15a
Fabio Leforte <fmatias@indracompany.com>
2018-03-17 10:51:24 -0300

    Enviado o projeto para o repositório

---
+++ client/app/app.module.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+
+
+
+@NgModule({
+    imports: [BrowserModule, FotoModule, HttpModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+
+})
+export class AppModule { }
+

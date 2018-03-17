f0fdb3c633215dfd4bb8c79d6a9d769e7f4a22ae
Fabio Leforte <fmatias@indracompany.com>
2018-03-17 12:24:08 -0300

    atualação do painel

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
+import 'rxjs/add/operator/map';
 
 
 
 @NgModule({
-    imports: [BrowserModule, FotoModule, HttpModule],
+    imports: [BrowserModule, FotoModule, HttpModule, PainelModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 

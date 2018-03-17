6a0672520c22463ab2f574c4f55c7e3a8beac908
Fabio Leforte <fmatias@indracompany.com>
2018-03-17 13:45:17 -0300

    efetuado a criação do cadastro e listagem

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
@@ -6,12 +5,14 @@ import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
 import 'rxjs/add/operator/map';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 
 
 @NgModule({
     imports: [BrowserModule, FotoModule, HttpModule, PainelModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, CadastroComponent, ListagemComponent],
     bootstrap: [AppComponent]
 
 })

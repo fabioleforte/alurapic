7a83705a3ba3e1e792d9715199aa7b660bd67544
Fabio Leforte <fmatias@indracompany.com>
2018-03-25 13:54:56 -0300

    Concluído o primeiro modulo

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -20,8 +20,8 @@ export class CadastroComponent {
     constructor(http: Http, fb: FormBuilder) {
         this.http = http;
         this.meuForm = fb.group({
-            titulo: [ '', Validators.compose([ Validators.required, Validators.minLength(4) ]) ],
-            url: [ '', Validators.required ],
+            titulo: ['',Validators.compose([Validators.required, Validators.minLength(4)])],
+            url: ['',Validators.required ],
             descicao: [ '' ]
         })
 
